3fb3e4f2f713094cb408b7430e844a52d436c5da
Luiz <luizotcunha@gmail.com>
2021-06-08 11:08:02 -0400

    alterando string de conexão dentro do appsettings

--- README.md
+++ README.md
@@ -40,7 +40,10 @@ npm start
 Requerido [DotNetCore](https://dotnet.microsoft.com/download/dotnet/5.0) v5 para rodar.
 Backend foi desenvolvido no Visual Studio 2019 Community Edition.
 
-1) Altere a string de conexão com o banco de dados;
+1) Altere a string de conexão com o banco de dados(é recomendado colocar os dados referentes ao sa, para que no update crie todas as tabelas corretamente). Ela fica no arquivo dentro do projeto fullstack-challenge:
+   ```sh
+appsettings.json
+```
 2) Sete fullstack-challenge como projeto de inicialização;
 
 
--- fullstack-challenge/fullstack-challeng.data/Context/Mycontext.cs
+++ fullstack-challenge/fullstack-challeng.data/Context/Mycontext.cs
@@ -1,18 +1,31 @@
 ﻿using fullstack_challenge.domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Microsoft.Extensions.Configuration;
+using System.Xml;
+using System.IO;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace fullstack_challeng.data.Context
 {
     public class Mycontext : DbContext
     {
+
         public DbSet<Classified> Classifieds { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = localhost; Database = fullstack-challenge; User Id = sa; Password = 1q2w3e4r@#$");
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) // <== compile failing here
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
         }
     }
+
 }
